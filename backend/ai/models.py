e09221fbb66d78a416be4fb6ac068d17cb589c46
DEENUU1 <kacperwlodarczyk@protonmail.com>
2024-04-18 01:17:52 +0200

    feature: create input/output schemas for InputData model

--- backend/ai/models.py
+++ backend/ai/models.py
@@ -1,3 +1,31 @@
+import uuid
 from django.db import models
+from utils.base_model import BaseModel
 
-# Create your models here.
+
+class InputData(BaseModel):
+    SOURCE = (
+        ('YOUTUBE', 'YOUTUBE'),
+        ('SPOTIFY', 'SPOTIFY'),
+    )
+    TRANSCRIPTION_TYPE = (
+        ("GENERATED", "GENERATED"),
+        ("MANUAL", "MANUAL"),
+        ("LLM", "LLM"),
+    )
+    STATUS = (
+        ("NEW", "NEW"),
+        ("PROCESSING", "PROCESSING"),
+        ("DONE", "DONE"),
+        ("ERROR", "ERROR"),
+    )
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    source = models.CharField(max_length=25, choices=SOURCE, null=False)
+    transcription_type = models.CharField(max_length=25, choices=TRANSCRIPTION_TYPE, null=False)
+    audio_length = models.IntegerField(null=True, blank=True)  # Value in seconds
+    source_title = models.CharField(max_length=255, null=True, blank=True)
+    source_url = models.URLField(null=True, blank=True)
+    status = models.CharField(max_length=25, choices=STATUS, null=False, default="NEW")
+
+
+
---
+++ backend/ai/serializers.py
@@ -0,0 +1,25 @@
+from .models import InputData
+from rest_framework.serializers import ModelSerializer
+
+
+class InputDataInputSerializer(ModelSerializer):
+    class Meta:
+        model = InputData
+        fields = (
+            "source",
+            "source_url",
+        )
+
+
+class InputDataOutputSerializer(ModelSerializer):
+    class Meta:
+        model = InputData
+        fields = (
+            "id",
+            "source",
+            "transcription_type",
+            "audio_length",
+            "source_title",
+            "source_url",
+            "status"
+        )
