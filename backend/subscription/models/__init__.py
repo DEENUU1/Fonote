7488852d8998a33baa87f17268177b316064298f
DEENUU1 <kacperwlodarczyk@protonmail.com>
2024-04-18 18:35:48 +0200

    feature: add UserSubscription model

--- backend/subscription/models/__init__.py
+++ backend/subscription/models/__init__.py
@@ -1,2 +1,3 @@
 from .plan import Plan
 from .price import Price
+from .user_subscription import UserSubscription
---
+++ backend/subscription/models/user_subscription.py
@@ -0,0 +1,19 @@
+from django.db import models
+from utils.base_model import BaseModel
+from django.contrib.auth.backends import UserModel
+from .plan import Plan
+
+
+class UserSubscription(BaseModel):
+    STATUS = (
+        ("ACTIVE", "ACTIVE"),
+        ("CANCELED", "CANCELED"),
+        ("EXPIRED", "EXPIRED"),
+    )
+
+    user = models.OneToOneField(UserModel, on_delete=models.SET_NULL, null=True)
+    subscription_id = models.CharField(max_length=255, unique=True)
+    plan = models.OneToOneField(Plan, on_delete=models.SET_NULL, null=True)
+    start_date = models.DateField(null=False)
+    end_date = models.DateField(null=False)
+    status = models.CharField(max_length=25, choices=STATUS, default="ACTIVE")
