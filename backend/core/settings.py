1c3192f446c443d3b4c9404f2fc3aedf4f62ce90
DEENUU1 <kacperwlodarczyk@protonmail.com>
2024-04-18 15:59:11 +0200

    feature: init subscription application

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -57,6 +57,7 @@ INSTALLED_APPS = [
     # project apps
     'authentication',
     'ai',
+    'subscription',
 ]
 
 MIDDLEWARE = [
--- backend/core/urls.py
+++ backend/core/urls.py
@@ -4,6 +4,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('api/auth/', include('authentication.urls')),
+    path('api/subscription/', include('subscription.urls')),
     path('api/ai/', include('ai.urls')),
     path("admin/", admin.site.urls),
 ]
