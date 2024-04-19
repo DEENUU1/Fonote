1e837767c6a1a03f4f57e2707495e2aa6a78fc0d
DEENUU1 <kacperwlodarczyk@protonmail.com>
2024-04-19 16:20:17 +0200

    refactor: create Admin classes for Order and UserSubscription models

--- backend/subscription/admin.py
+++ backend/subscription/admin.py
@@ -11,7 +11,16 @@ class PriceAdmin(admin.ModelAdmin):
     list_display = ("price", "prev_price")
 
 
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ("user", "total_amount", "invoice_url")
+
+
+class UserSubscriptionAdmin(admin.ModelAdmin):
+    list_display = ("user", "plan", "start_date", "end_date", "status", "order")
+    list_filter = ("status", "plan")
+
+
 admin.site.register(plan.Plan, PlanAdmin)
 admin.site.register(price.Price, PriceAdmin)
-admin.site.register(user_subscription.UserSubscription)
-admin.site.register(order.Order)
+admin.site.register(user_subscription.UserSubscription, UserSubscriptionAdmin)
+admin.site.register(order.Order, OrderAdmin)
