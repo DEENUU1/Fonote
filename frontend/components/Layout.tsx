ffb094ce530b3e1d338d932dd1459d000c17323b
DEENUU1 <kacperwlodarczyk@protonmail.com>
2024-04-21 16:44:19 +0200

    style: add basic Navbar for navigation : :

--- frontend/components/Layout.tsx
+++ frontend/components/Layout.tsx
@@ -1,10 +1,15 @@
+import Navbar from "@/components/Navbar";
+
 
 // @ts-ignore
 export default function Layout({ children }) {
   return (
+    <>
+    <Navbar/>
     <section className="text-gray-700 body-font overflow-hidden border-t border-gray-200">
       <main>{children}</main>
     </section>
+</>
 
   )
 }
