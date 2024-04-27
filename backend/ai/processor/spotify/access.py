eeebc291d2ddc77308b404239e20ae00c2425725
DEENUU1 <kacperwlodarczyk@protonmail.com>
2024-04-27 17:31:59 +0200

    feature: create SpotifyAccess class

---
+++ backend/ai/processor/spotify/access.py
@@ -0,0 +1,50 @@
+import base64
+import requests
+import json
+import logging
+from django.conf import settings
+
+
+logger = logging.getLevelName(__name__)
+
+
+class SpotifyAccess:
+    def __init__(self):
+        self.client_id: str = settings.SPOTIFY_CLIENT_ID
+        self.client_secret: str = settings.SPOTIFY_CLIENT_SECRET
+        self.base_url = "https://accounts.spotify.com/api/token"
+
+    def get_token(self) -> str:
+        auth_string = f'{self.client_id}:{self.client_secret}'
+        auth_bytes = auth_string.encode('utf-8')
+        auth_base64 = str(base64.b64encode(auth_bytes), "utf-8")
+
+        headers = {
+            "Authorization": "Basic " + auth_base64,
+            "Content-Type": "application/x-www-form-urlencoded"
+        }
+
+        data = {"grant_type": "client_credentials"}
+
+        try:
+            result = requests.post(self.base_url, headers=headers, data=data)
+            result.raise_for_status()
+
+            json_result = json.loads(result.content)
+            token = json_result["access_token"]
+            return token
+
+        except requests.exceptions.HTTPError as err:
+            logger.error(f"HTTPError {err}")
+
+        except requests.exceptions.RequestException as err:
+            logger.error(f"RequestException {err}")
+
+        except Exception as err:
+            logger.error(f"Exception {err}")
+
+
+if __name__ == '__main__':
+    access = SpotifyAccess()
+    token = access.get_token()
+    print(token)
