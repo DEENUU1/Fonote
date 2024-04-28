3064446d1f28169733db572df462b0c845c7728d
DEENUU1 <kacperwlodarczyk@protonmail.com>
2024-04-28 17:06:31 +0200

    bugfix: database configuration

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -105,13 +105,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-if WORK_MODE == "DEV":
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
+}
 
 
 # Password validation
